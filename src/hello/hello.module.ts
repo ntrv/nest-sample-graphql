139eee86fb65fcadc348f4d8eaa19b44652ad38d
ntrv <hydrangea56@me.com>
2019-08-16 02:14:53 +0900

    Use ConfigService

--- src/hello/hello.module.ts
+++ src/hello/hello.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HelloController } from './hello.controller';
 import { HelloService } from './hello.service';
+import { ConfigModule } from '../config/config.module';
 
 @Module({
+  imports: [ConfigModule],
   controllers: [HelloController],
   providers: [HelloService]
 })
--- src/hello/hello.service.ts
+++ src/hello/hello.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '../config/config.service';
 
 @Injectable()
 export class HelloService {
+  constructor(private readonly config: ConfigService){}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getDBUser(): string {
+    return this.config.get('DB_USER');
   }
 }
