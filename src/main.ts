664a4969bc62ef48a58216222646d8a39c9a127e
ntrv <hydrangea56@me.com>
2019-08-18 03:10:59 +0900

    Add logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
+  const port = process.env.APP_PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.APP_PORT || 3000);
+  await app.listen(port);
+  Logger.log(`🚀 Server running on http://localhost:${port}`, 'Bootstrap');
 }
 bootstrap();
