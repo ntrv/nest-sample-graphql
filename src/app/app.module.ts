64d37a43c88f8df9bc70806f4d2577029decf1f0
ntrv <hydrangea56@me.com>
2019-08-18 02:14:55 +0900

    Install GraphQL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { ConfigModule } from '../config/config.module';
 import { HelloModule } from '../hello/hello.module';
+import { join } from 'path';
 
 @Module({
-  imports: [ConfigModule, HelloModule],
+  imports: [
+    ConfigModule,
+    HelloModule,
+    GraphQLModule.forRoot({
+      typePaths: ['../**.*.graphql'],
+      definitions: {
+        path: join(__dirname, '../graphql.ts'),
+        outputAs: 'class',
+      }
+    }),
+  ],
 })
 export class AppModule {}
