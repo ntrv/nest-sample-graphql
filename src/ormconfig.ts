df77e5df93bfb4e0a8a8d0c696c272bb2adee6d4
ntrv <hydrangea56@me.com>
2019-08-18 23:07:31 +0900

    Fix typo

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -3,13 +3,13 @@ import { ConfigService } from './config/config.service';
 import { join } from 'path';
 
 const config = new ConfigService(process.env.NODE_ENV);
-const ormConfig: ConnectionOptions = {
+export const ormConfig: ConnectionOptions = {
   type: config.getOrFail('DB_CONNECTION') as any,
   host: config.getOrFail('DB_HOST'),
   port: parseInt(config.get('DB_PORT'), 10) || 3306,
   username: config.get('DB_USERNAME'),
   password: config.get('DB_PASSWORD'),
-  database: config.get('DB_NAME'),
+  database: config.get('DB_DATABASE'),
   entities: [join(__dirname, './**/*.entity{.ts,.js}')],
   synchronize: false,
   migrationsRun: false,
@@ -20,5 +20,3 @@ const ormConfig: ConnectionOptions = {
     migrationsDir: join(__dirname, '../migrations'),
   },
 };
-
-export default ormConfig;
